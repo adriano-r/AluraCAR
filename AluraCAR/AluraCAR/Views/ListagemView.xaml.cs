eea97c5998124a2b6ce33e4062754aad47eb2966
adriano <adrianorosa9974@gmail.com>
2023-05-11 17:36:42 -0300

    Add project files.

---
+++ AluraCAR/AluraCAR/Views/ListagemView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AluraCAR.Views
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+    }
+
+    public partial class ListagemView : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+        public ListagemView()
+        {
+            InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo { Nome = "Azire", Preco = 60000 },
+                new Veiculo { Nome = "Gol", Preco = 40000 },
+                new Veiculo { Nome = "Corsa", Preco = 30000 }
+            };
+
+            this.BindingContext = this;
+        }
+
+        private void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            Navigation.PushAsync(new DetalheView(veiculo));
+        }
+    }
+}
